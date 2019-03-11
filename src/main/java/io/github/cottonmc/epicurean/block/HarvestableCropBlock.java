00eed863b9b088930f410e7d13568173accf0a19
Meredith Espinosa <goclonefilms@gmail.com>
2019-03-11 04:09:58 -0700

    fix crash on right-click-harvesting crops

--- src/main/java/io/github/cottonmc/epicurean/block/HarvestableCropBlock.java
+++ src/main/java/io/github/cottonmc/epicurean/block/HarvestableCropBlock.java
@@ -21,10 +21,12 @@ import java.util.List;
 
 public class HarvestableCropBlock extends CropBlock {
 	public final Item cropItem;
+	private final int resetGrowthTo;
 
-	public HarvestableCropBlock(Item cropItem) {
+	public HarvestableCropBlock(Item cropItem, int resetGrowthTo) {
 		super(FabricBlockSettings.of(Material.PLANT).sounds(BlockSoundGroup.CROP).ticksRandomly().breakInstantly().build());
 		this.cropItem = cropItem;
+		this.resetGrowthTo = resetGrowthTo;
 	}
 
 	@Override
@@ -34,6 +36,7 @@ public class HarvestableCropBlock extends CropBlock {
 
 	@Override
 	public boolean activate(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+		if (world.isClient) return true;
 		if (state.get(CropBlock.AGE) == 7) {
 			LootContext.Builder ctx = new LootContext
 					.Builder(world.getServer().getWorld(world.dimension.getType()))
@@ -43,7 +46,7 @@ public class HarvestableCropBlock extends CropBlock {
 				ItemEntity entity = new ItemEntity(world, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, stack);
 				world.spawnEntity(entity);
 			}
-			world.setBlockState(pos, state.with(CropBlock.AGE, 4));
+			world.setBlockState(pos, state.with(CropBlock.AGE, resetGrowthTo));
 			return true;
 		}
 		return false;
