909c75d6e8782ecf6b16120c5f27233e0cd8b0fd
Meredith Espinosa <goclonefilms@gmail.com>
2019-03-29 22:02:08 -0700

    update how to get salt

--- src/main/java/io/github/cottonmc/epicurean/mixins/MixinCauldron.java
+++ src/main/java/io/github/cottonmc/epicurean/mixins/MixinCauldron.java
@@ -3,11 +3,15 @@ package io.github.cottonmc.epicurean.mixins;
 import io.github.cottonmc.epicurean.item.EpicureanItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.CauldronBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
@@ -29,12 +33,19 @@ public abstract class MixinCauldron extends Block {
 	public void harvestSalt(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult result, CallbackInfoReturnable cir,
 							ItemStack stack, int level) {
 		Item item = stack.getItem();
-		if (item == Items.STICK) {
-			if (level > 0 && !world.isClient) {
-				ItemStack salt = new ItemStack(EpicureanItems.SALT, 1);
-				if (!player.inventory.insertStack(salt)) {
-					player.dropItem(salt, false);
+		if (item == Items.NETHER_WART) {
+			if (level > 0 && !world.isClient && world.getBlockState(pos.down()).getBlock() == Blocks.FIRE) {
+				if (!player.abilities.creativeMode) {
+					ItemStack salt = new ItemStack(EpicureanItems.SALT, 1);
+					player.increaseStat(Stats.USE_CAULDRON);
+					stack.subtractAmount(1);
+					if (stack.isEmpty()) {
+						player.setStackInHand(hand, salt);
+					} else if (!player.inventory.insertStack(salt)) {
+						player.dropItem(salt, false);
+					}
 				}
+				world.playSound(null, pos, SoundEvents.BLOCK_NETHER_WART_BREAK, SoundCategory.BLOCK, 1.0f, 1.0f);
 				((CauldronBlock)(Object)this).setLevel(world, pos, state, level - 1);
 				cir.setReturnValue(true);
 			}
