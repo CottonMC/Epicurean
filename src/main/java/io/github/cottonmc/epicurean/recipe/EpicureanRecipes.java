7bde275e508466945a769d29873d61642d7777ad
Meredith Espinosa <goclonefilms@gmail.com>
2019-04-04 16:33:18 -0700

    update cotton and cauldron behavior

--- src/main/java/io/github/cottonmc/epicurean/recipe/EpicureanRecipes.java
+++ src/main/java/io/github/cottonmc/epicurean/recipe/EpicureanRecipes.java
@@ -1,11 +1,12 @@
 package io.github.cottonmc.epicurean.recipe;
 
+import io.github.cottonmc.cotton.cauldron.Cauldron;
 import io.github.cottonmc.cotton.cauldron.CauldronBehavior;
-import io.github.cottonmc.cotton.cauldron.CauldronUtils;
 import io.github.cottonmc.epicurean.EpicureanGastronomy;
 import io.github.cottonmc.epicurean.item.EpicureanItems;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.Recipe;
@@ -43,7 +44,7 @@ public class EpicureanRecipes {
 
 	public static void init() {
 		CauldronBehavior.registerBehavior(
-				(ctx) -> FluidTags.WATER.contains(ctx.getCauldronFluid())
+				(ctx) -> FluidTags.WATER.contains(ctx.getFluid())
 						&& ctx.getStack().getItem() == Items.NETHER_WART
 						&& ctx.getWorld().getBlockState(ctx.getPos().down()).getBlock() == Blocks.FIRE
 						&& !ctx.getWorld().isClient,
@@ -60,7 +61,7 @@ public class EpicureanRecipes {
 							player.dropItem(salt, false);
 						}
 					}
-					CauldronUtils.tryDrainFluid(ctx.getWorld(), ctx.getPos(), ctx.getState());
+					((Cauldron)ctx.getState().getBlock()).drain(ctx.getWorld(), ctx.getPos(), ctx.getState(), Fluids.WATER, 1);
 					ctx.getWorld().playSound(null, ctx.getPos(), SoundEvents.BLOCK_NETHER_WART_BREAK, SoundCategory.BLOCK, 1.0f, 1.0f);
 				});
 	}
