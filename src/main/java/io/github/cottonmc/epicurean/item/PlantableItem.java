cf1506d421a62fc84ad8bd9fdfdae7626c6c6fa9
Meredith Espinosa <goclonefilms@gmail.com>
2019-04-23 11:38:39 -0700

    update to full 1.14!

--- src/main/java/io/github/cottonmc/epicurean/item/PlantableItem.java
+++ src/main/java/io/github/cottonmc/epicurean/item/PlantableItem.java
@@ -1,12 +1,12 @@
 package io.github.cottonmc.epicurean.item;
 
 import net.minecraft.block.Block;
+import net.minecraft.item.AliasedBlockItem;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.StringItem;
 import net.minecraft.util.DefaultedList;
 
-public class PlantableItem extends StringItem {
+public class PlantableItem extends AliasedBlockItem {
 
 	public PlantableItem(Block block, Settings settings) {
 		super(block, settings);
