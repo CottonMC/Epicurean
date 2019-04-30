572eb9eb3c836e36cb714f55fdf58a367a2ace97
Meredith Espinosa <goclonefilms@gmail.com>
2019-04-29 20:52:35 -0700

    update mod menu

--- src/main/java/io/github/cottonmc/epicurean/EpicureanClient.java
+++ src/main/java/io/github/cottonmc/epicurean/EpicureanClient.java
@@ -2,6 +2,7 @@ package io.github.cottonmc.epicurean;
 
 import io.github.cottonmc.epicurean.container.CookingTableScreen;
 import io.github.cottonmc.epicurean.block.EpicureanBlocks;
+import io.github.prospector.modmenu.api.ModMenuApi;
 import me.shedaniel.cloth.api.ConfigScreenBuilder;
 import me.shedaniel.cloth.gui.ClothConfigScreen;
 import me.shedaniel.cloth.gui.entries.*;
@@ -10,40 +11,42 @@ import net.fabricmc.fabric.api.client.screen.ScreenProviderRegistry;
 import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.Screen;
 import net.minecraft.util.Identifier;
 
-import java.lang.reflect.Method;
+import java.util.function.Function;
 
-public class EpicureanClient implements ClientModInitializer {
+public class EpicureanClient implements ClientModInitializer, ModMenuApi {
 
 	@Override
 	public void onInitializeClient() {
 		ClientSpriteRegistryCallback.EVENT.register(((spriteAtlasTexture, registry) -> registry.register(new Identifier(EpicureanGastronomy.MOD_ID, "gui/cooking_table"))));
 
 		ScreenProviderRegistry.INSTANCE.registerFactory(EpicureanBlocks.COOKING_CONTAINER, (syncId, identifier, player, buf) -> new CookingTableScreen(syncId, player));
-		if (FabricLoader.getInstance().isModLoaded("modmenu")) {
-			try {
-				Class<?> clazz = Class.forName("io.github.prospector.modmenu.api.ModMenuApi");
-				Method method = clazz.getMethod("addConfigOverride", String.class, Runnable.class);
-				method.invoke(null, "epicurean", (Runnable) () -> {
-					ClothConfigScreen.Builder builder = new ClothConfigScreen.Builder(MinecraftClient.getInstance().currentScreen, "Epicurean Gastronomy Config", null);
-					builder.addCategories("Primary Features", "Fun Features");
-					ConfigScreenBuilder.CategoryBuilder primaryFeatures = builder.getCategory("Primary Features");
-					primaryFeatures.addOption(new BooleanListEntry("Hopper Harvesting", EpicureanGastronomy.config.hopperHarvest, "text.cloth-config.reset_value", () -> false, null));
-					primaryFeatures.addOption(new BooleanListEntry("Saturation Only", EpicureanGastronomy.config.useSaturationOnly, "text.cloth-config.reset_value", () -> false, null));
-					primaryFeatures.addOption(new IntegerListEntry("Natural Regen Speed", EpicureanGastronomy.config.naturalRegenSpeed, "text.cloth-config.reset_value", () -> 15, null));
-					primaryFeatures.addOption(new DoubleListEntry("Seasoning Efficiency", EpicureanGastronomy.config.seasoningEfficiency, "text.cloth-config.reset_value", () -> 0.3d, null));
-					ConfigScreenBuilder.CategoryBuilder funFeatures = builder.getCategory("Fun Features");
-					funFeatures.addOption(new BooleanListEntry("Edible Nuggets", EpicureanGastronomy.config.edibleNuggets, "text.cloth-config.reset_value", () -> false, null));
-					funFeatures.addOption(new BooleanListEntry("Omnivore Mode", EpicureanGastronomy.config.omnivoreEnabled, "text.cloth-config.reset_value", () -> false, null));
-					funFeatures.addOption(new IntegerSliderEntry("Omnivore Hunger", 0, 20, EpicureanGastronomy.config.omnivoreFoodRestore, "text.cloth-config.reset_value", () -> 2, null));
-					funFeatures.addOption(new FloatListEntry("Omnivore Saturation", EpicureanGastronomy.config.omnivoreSaturationRestore, "text.cloth-config.reset_value", () -> 0.25f, null));
-					funFeatures.addOption(new DoubleListEntry("Omnivore Item Damage", EpicureanGastronomy.config.omnivoreItemDamage, "text.cloth-config.reset_value", () -> 30d, null));
-					MinecraftClient.getInstance().openScreen(builder.build());
-				});
-			} catch (Exception e) {
-				EpicureanGastronomy.LOGGER.error("Failed to add config screen to Mod Menu", e);
-			}
-		}
+	}
+
+	@Override
+	public String getModId() {
+		return EpicureanGastronomy.MOD_ID;
+	}
+
+	@Override
+	public Function<Screen, ? extends Screen> getConfigScreenFactory() {
+		return (prevScreen) -> {
+			ClothConfigScreen.Builder builder = new ClothConfigScreen.Builder(MinecraftClient.getInstance().currentScreen, "Epicurean Gastronomy Config", null);
+			builder.addCategories("Primary Features", "Fun Features");
+			ConfigScreenBuilder.CategoryBuilder primaryFeatures = builder.getCategory("Primary Features");
+			primaryFeatures.addOption(new BooleanListEntry("Hopper Harvesting", EpicureanGastronomy.config.hopperHarvest, "text.cloth-config.reset_value", () -> false, null));
+			primaryFeatures.addOption(new BooleanListEntry("Saturation Only", EpicureanGastronomy.config.useSaturationOnly, "text.cloth-config.reset_value", () -> false, null));
+			primaryFeatures.addOption(new IntegerListEntry("Natural Regen Speed", EpicureanGastronomy.config.naturalRegenSpeed, "text.cloth-config.reset_value", () -> 15, null));
+			primaryFeatures.addOption(new DoubleListEntry("Seasoning Efficiency", EpicureanGastronomy.config.seasoningEfficiency, "text.cloth-config.reset_value", () -> 0.3d, null));
+			ConfigScreenBuilder.CategoryBuilder funFeatures = builder.getCategory("Fun Features");
+			funFeatures.addOption(new BooleanListEntry("Edible Nuggets", EpicureanGastronomy.config.edibleNuggets, "text.cloth-config.reset_value", () -> false, null));
+			funFeatures.addOption(new BooleanListEntry("Omnivore Mode", EpicureanGastronomy.config.omnivoreEnabled, "text.cloth-config.reset_value", () -> false, null));
+			funFeatures.addOption(new IntegerSliderEntry("Omnivore Hunger", 0, 20, EpicureanGastronomy.config.omnivoreFoodRestore, "text.cloth-config.reset_value", () -> 2, null));
+			funFeatures.addOption(new FloatListEntry("Omnivore Saturation", EpicureanGastronomy.config.omnivoreSaturationRestore, "text.cloth-config.reset_value", () -> 0.25f, null));
+			funFeatures.addOption(new DoubleListEntry("Omnivore Item Damage", EpicureanGastronomy.config.omnivoreItemDamage, "text.cloth-config.reset_value", () -> 30d, null));
+			return builder.build();
+		};
 	}
 }
